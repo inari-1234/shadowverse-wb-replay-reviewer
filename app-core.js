f1d8d0a60ee9c2bece9cba4650b15ddbc64460d8
inari-1234 <shouri95@yahoo.co.jp>
2026-09-18 14:48:48 +0900

    Bump clean 13.3 app metadata

--- app-core.js
+++ app-core.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const APP={version:'4.13.2',build:'4.13.2-20260918-clean-13-2',revision:'clean-13-2'};
+const APP={version:'4.13.3',build:'4.13.3-20260918-clean-13-3',revision:'clean-13-3'};
 const WB=window.WB={APP,modules:[],events:[],errors:[],readyQueue:[],ready:false,video:null,videoMeta:null,videoName:'replay',objectUrl:null,turnTimeline:[],turnValidation:null,mulligan:null,classDetection:null,stateCapture:null,scenes:[],seekCount:0,seekReasons:{},task:null,cancelRequested:false,swInfo:null};
 WB.$=s=>document.querySelector(s);
 WB.registerModule=(name,version)=>{const row={name,version};if(!WB.modules.some(x=>x.name===name))WB.modules.push(row);return row};
